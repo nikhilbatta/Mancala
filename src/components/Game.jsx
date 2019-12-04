e5d0fbd0222497e52c2073efbaf2d8e574b80632
Student <example@example.com>
2019-12-04 10:58:20 -0800

    Add logic to sow seeds from pocket and update scores

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -5,8 +5,34 @@ function Game(){
     const [player1Score, setScoreP1] = useState(0)
     const [player2Score, setScoreP2] = useState(0)
     const [board, setBoard] = useState([[4,4,4,4],[4,4,4,4]])
+    
     function handlePocketClick(id){
-        console.log(id)
+        let row = Number(id.substring(0,1));
+        const activePlayer = row;
+        let index = Number(id.substring(id.indexOf('-')+1));
+        console.log(board);
+        const pocketValue = board[row][index];
+        const removedForScore = Math.floor(index/4);
+        const addToBothScores = Math.floor(removedForScore/2);
+        const addToActivePlayer = removedForScore % 2;
+        const forPlayArea = pocketValue - removedForScore;
+        board[row][index] = 0;
+        for(let i = 0; i < forPlayArea; i++){
+            index += 1;
+            if(index % 4 === 0){
+                index = 0;
+                row = (row +1) % 2;
+            }
+            board[row][index] = board[row][index] + 1;
+        }
+        if(activePlayer === 0){
+            setScoreP1(player1Score + addToBothScores);
+            setScoreP2(player2Score + addToBothScores + addToActivePlayer);
+        } else {
+            setScoreP1(player1Score + addToBothScores + addToActivePlayer);
+            setScoreP2(player2Score + addToBothScores);
+        }
+        setBoard(board);
     }
     return (
         <div>
